fa2de59dbf897649a09a90b765a8f98b3731d0eb
mattsparks77 <34433294+mattsparks77@users.noreply.github.com>
2018-11-19 20:56:56 -0800

    updated player controller and added input manager

---
+++ 3 Barrel Shooter/Assets/InputManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour {
+    // axis
+    public static float MainHorizontal()
+    {
+        float r = 0.0f;
+        r += Input.GetAxisRaw("J_MainHorizontal");
+        r += Input.GetAxisRaw("K_MainHorizontal");
+        return Mathf.Clamp(r, -1.0f, 1.0f);
+
+    }
+    public static float MainVertical()
+    {
+        float r = 0.0f;
+        r += Input.GetAxisRaw("J_MainVertical");
+        r += Input.GetAxisRaw("K_MainVertical");
+        return Mathf.Clamp(r, -1.0f, 1.0f);
+
+    }
+    public static Vector2 MainJoystick()
+    {
+        return new Vector2(MainHorizontal(), MainVertical());
+    }
+
+
+        //buttons
+    public static bool vacuumButton()
+    {
+        return Input.GetButtonDown("VacuumButton");
+    }
+    public static bool shootButton()
+    {
+        return Input.GetButtonDown("ShootButton");
+    }
+
+    
+
+}
--- 3 Barrel Shooter/Assets/Scripts/PlayerController.cs
+++ 3 Barrel Shooter/Assets/Scripts/PlayerController.cs
@@ -10,15 +10,14 @@ public class PlayerController : MonoBehaviour
     private bool flipped = false;
     [SerializeField]
     private float speed =3f;
- 
+    public string inputHorizontal = "Horizontal";
+    public string inputVertical = "Vertical";
+    public string shootButton = "ShootButton_P1";
+    public string vacuumButton = "VacuumButton_P1";
     float horizontal;
     float vertical;
  
 
-    void Start()
-    {
-     
-    }
 
     void Update()
     {
@@ -26,66 +25,91 @@ public class PlayerController : MonoBehaviour
         vertical = Input.GetAxisRaw("Vertical");
     }
 
-    void FixedUpdate()
-    {
-        Move();
-    }
+    //void FixedUpdate()
+    //{
+    //    Move();
+    //}
     public void Move()
     {
         Vector2 movement = new Vector2(horizontal * speed, vertical * speed);
         transform.Translate(movement * Time.deltaTime);
     }
 
-        //   // Use this for initialization
-        //   void Start () {
 
-        //}
 
-        //// Update is called once per frame
-        //void Update () {
-        //       GetInput();
-        //}
 
-        //   public void Move(Vector2 direction)
-        //   {
-        //       transform.Translate(direction * speed * Time.deltaTime);
-        //	if (flipped) {
-        //		flipped = false;
-        //           playerBody.transform.Rotate(0f, 180f, 0f);
-        //	}
 
-        //   }
 
-        //   private void GetInput(){
+    //void Update()
+    //{
+    //    float horizontal = Input.GetAxisRaw(inputHorizontal);
+    //    float vertical = Input.GetAxisRaw(inputVertical);
+    //    Debug.Log("Vert: " + vertical.ToString() + "Horizont: " + horizontal.ToString());
 
-        //       if (Input.GetKey(KeyCode.W)){
-        //           Move(Vector2.up);
-        //       }
 
-        //       if (Input.GetKey(KeyCode.A))
-        //       {
-        //		if (facingRight) {
-        //			facingRight = false;
-        //			flipped = true;
-        //		}
-        //		Move(Vector2.left);
+    //}
 
-        //       }
+    void FixedUpdate()
+    {
+        Move();
+    }
+    //public void Move()
+    //{
+    //    Vector2 movement = new Vector2(horizontal * speed, vertical * speed);
+    //    transform.Translate(movement * Time.deltaTime, Space.World);
 
-        //       if (Input.GetKey(KeyCode.D))
-        //       {
-        //		if (!facingRight) {
-        //			facingRight = true;
-        //			flipped = true;
-        //		}
-        //           Move(Vector2.right);
 
-        //       }
+    //}
+    //   // Use this for initialization
+    //   void Start () {
 
-        //       if (Input.GetKey(KeyCode.S))
-        //       {
-        //           Move(Vector2.down);
-        //       }
-        //   }
-    
+    //}
+
+    //// Update is called once per frame
+    //void Update () {
+    //       GetInput();
+    //}
+
+    //   public void Move(Vector2 direction)
+    //   {
+    //       transform.Translate(direction * speed * Time.deltaTime);
+    //	if (flipped) {
+    //		flipped = false;
+    //           playerBody.transform.Rotate(0f, 180f, 0f);
+    //	}
+
+    //   }
+
+    //   private void GetInput(){
+
+    //       if (Input.GetKey(KeyCode.W)){
+    //           Move(Vector2.up);
+    //       }
+
+    //       if (Input.GetKey(KeyCode.A))
+    //       {
+    //		if (facingRight) {
+    //			facingRight = false;
+    //			flipped = true;
+    //		}
+    //		Move(Vector2.left);
+
+    //       }
+
+    //       if (Input.GetKey(KeyCode.D))
+    //       {
+    //		if (!facingRight) {
+    //			facingRight = true;
+    //			flipped = true;
+    //		}
+    //           Move(Vector2.right);
+
+    //       }
+
+    //       if (Input.GetKey(KeyCode.S))
+    //       {
+    //           Move(Vector2.down);
+    //       }
+    //   }
+
 }
