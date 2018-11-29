db6e8d60fdc6ea7a223549f5c32cd257fdd9c55c
Matt Sparks <masparks@uci.edu>
2018-11-29 10:20:07 -0800

    mappings may be wonky on other comps
    but the joysticks should work and 360 rotation and i mapped sucking and shooting to clikcing the joysticks respectively. will change later to l/r trigger

--- 3 Barrel Shooter/Assets/Scripts/PlayerController.cs
+++ 3 Barrel Shooter/Assets/Scripts/PlayerController.cs
@@ -15,8 +15,8 @@ public class PlayerController : MonoBehaviour
     public string inputVertical = "Vertical";
     public string shootButton = "ShootButton_P1";
     public string vacuumButton = "VacuumButton_P1";
-    float l_horizontal;
-    float l_vertical;
+    float horizontal;
+    float vertical;
     bool suck;
     bool shoot;
     float last_heading;
@@ -44,22 +44,41 @@ public class PlayerController : MonoBehaviour
     public void UpdatePlayerMovement(ControllerInputs inputs)
     {
         // Getting the horizontal axis
-        l_horizontal = inputs.Left_Stick_Horizontal;
-        l_vertical = -inputs.Left_Stick_Veritcal;
 
+
+        //float r_horizontal = inputs.Right_Stick_Horizontal;
+        //if (r_horizontal < 0)
+        //    transform.rotation = new Quaternion(0.0f, 0.0f, 90.0f, 1.0f);
+        //else if (r_horizontal > 0)
+        //    transform.rotation = new Quaternion(0.0f, 0.0f, 0.0f, 1.0f);
+
+        ////Change the position of the player
+        //Vector2 movement = new Vector2(l_horizontal * speed, l_vertical * speed);
+        //transform.Translate(movement * Time.deltaTime,Space.World);
+        horizontal = inputs.Left_Stick_Horizontal;
+        vertical = -inputs.Left_Stick_Veritcal;
+
+        float r_vertical = inputs.Right_Stick_Veritcal;
         float r_horizontal = inputs.Right_Stick_Horizontal;
-        if (r_horizontal < 0)
-            transform.rotation = new Quaternion(0.0f, 0.0f, 90.0f, 1.0f);
-        else if (r_horizontal > 0)
-            transform.rotation = new Quaternion(0.0f, 0.0f, 0.0f, 1.0f);
+        float heading = Mathf.Atan2(r_vertical, r_horizontal);
+
+
+
+        //Debug.Log(heading);
 
         //Change the position of the player
-        Vector2 movement = new Vector2(l_horizontal * speed, l_vertical * speed);
-        transform.Translate(movement * Time.deltaTime,Space.World);
-       
+        Vector2 movement = new Vector2(horizontal * speed, vertical * speed);
+        transform.Translate(movement * Time.deltaTime, Space.World);
+        //change rotation of pplayer if no input received keeps same rotation as last time it got input prevents snapping back to 0,0 heading
+        if (heading != 0)
+        {
+            float last_heading = heading;
+            transform.rotation = Quaternion.Euler(0f, 0f, last_heading * Mathf.Rad2Deg);
+        }
 
-       
 
-  
+
+
+
     }
 }
