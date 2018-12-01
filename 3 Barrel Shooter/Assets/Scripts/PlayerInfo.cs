005f45b7622655403ad2f0468c9a91d2ce38f800
Zachary Cloutier <zcloutie@uci.edu>
2018-12-01 15:47:42 -0800

    Fix code

--- 3 Barrel Shooter/Assets/Scripts/PlayerInfo.cs
+++ 3 Barrel Shooter/Assets/Scripts/PlayerInfo.cs
@@ -67,17 +67,21 @@ public class PlayerInfo : MonoBehaviour
 
     private IEnumerator respawn(){
         yield return new WaitForSeconds(1.0f);
-		Vector3 respawn;
+		Vector3 respawn = new Vector3(0,0,0);
         //Can specify respawn location before Coroutine is started and save as a temporary class variable
 		switch (rearea) {
 		case "1":
 			respawn = GetVector (4);
+			break;
 		case "2":
 			respawn = GetVector(3);
+			break;
 		case "3":
 			respawn = GetVector(2);
+			break;
 		case "4":
 			respawn = GetVector(1);
+			break;
 		}
 		transform.position = respawn;
         startedRespawn = !startedRespawn;
@@ -115,19 +119,24 @@ public class PlayerInfo : MonoBehaviour
 			return new Vector3(Random.Range(0,x_range),Random.Range(y_range,0f),-2f);
 		else if(x_range < 0f && y_range < 0f)
 			return new Vector3(Random.Range(x_range,0f),Random.Range(y_range,0f),-2f);
+		return new Vector3 (0, 0, 0);
 	}
 
 	private Vector3 GetVector(int area){
-		Vector3 re;
+		Vector3 re = new Vector3(0,0,0);
 		switch(area){
 		case 1:
 			re = GetRandomVector(-9,4);
+			break;
 		case 2:
 			re = GetRandomVector(9,4);
+			break;
 		case 3:
 			re = GetRandomVector(-9,-4);
+			break;
 		case 4:
 			re = GetRandomVector(9,-4);
+			break;
 		}
 		return re;
 	}
