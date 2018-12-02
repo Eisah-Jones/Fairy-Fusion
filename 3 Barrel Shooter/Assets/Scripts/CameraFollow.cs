848f92f41dd0812524d9ba8374c0196c89e95560
Zachary Cloutier <zcloutie@uci.edu>
2018-12-01 18:31:28 -0800

    CameraFollowPt2

---
+++ 3 Barrel Shooter/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	public GameObject player;
+	private Camera cam;
+	public int cam_num;
+	public int player_count;
+	// Use this for initialization
+	void Start () {
+		cam = GetComponent<Camera>();
+		switch(cam_num){
+		case 1:
+			if (player_count >= 1) {
+				player = GameObject.Find ("Player0");
+			} else
+				cam.enabled = false;
+			break;
+		case 2:
+			if (player_count >= 2) {
+				player = GameObject.Find ("Player1");
+			} else
+				cam.enabled = false;
+			break;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (player) {
+			Debug.Log ("WORKING");
+			transform.position.Set (player.transform.position.x, player.transform.position.y, transform.position.z);
+		}
+		else {
+			cam = GetComponent<Camera>();
+			switch(cam_num){
+			case 1:
+				if (player_count >= 1) {
+					player = GameObject.Find ("Player0");
+				} else
+					cam.enabled = false;
+				break;
+			case 2:
+				if (player_count >= 2) {
+					player = GameObject.Find ("Player1");
+				} else
+					cam.enabled = false;
+				break;
+			}
+		}
+	}
+}
