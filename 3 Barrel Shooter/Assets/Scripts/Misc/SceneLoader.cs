fa3ef76bc43ed232ff1f7cfc5f943b1f30b313cd
Zachary Cloutier <zcloutie@uci.edu>
2019-02-03 21:28:12 -0800

    Loading Screen

---
+++ 3 Barrel Shooter/Assets/Scripts/Misc/SceneLoader.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class SceneLoader : MonoBehaviour {
+	
+	private int dot_count = 1;
+	private bool loading = false;
+
+	[SerializeField]
+	private int scene;
+	[SerializeField]
+	private TextMeshProUGUI loadingText;
+	[SerializeField]
+	private int frame_count_to = 30;
+	private int frame_count = 0;
+
+
+	// Updates once per frame
+	void Update() {
+		//Starts loading scene if not already loading
+		if (loading == false) {
+			Debug.Log ("Loading Game");
+			StartCoroutine (LoadNewScene ());
+			loading = true;
+		}
+
+		//Adds dots to the end of text after a set amount of frames to let user know computer is working. After 3 it resets to 1
+		if (frame_count == frame_count_to) {
+			if (dot_count < 3) {
+				loadingText.text += ".";
+				dot_count += 1;
+			} else {
+				loadingText.text = "Loading.";
+				dot_count = 1;
+			}
+		} else if( frame_count > frame_count_to){
+			frame_count = 0;
+		}
+		frame_count += 1;
+		
+
+	}
+
+
+	// The coroutine runs on its own at the same time as Update() and takes an integer indicating which scene to load.
+	IEnumerator LoadNewScene() {
+		//Simulates a load, meant for debugging purposes, left in because the scene loads so fast there's no feedback to the player
+		yield return new WaitForSeconds(3);
+
+		// Start an asynchronous operation to load the scene that was passed to the LoadNewScene coroutine.
+		AsyncOperation async = Application.LoadLevelAsync(scene);
+
+		// While the asynchronous operation to load the new scene is not yet complete, continue waiting until it's done.
+		while (!async.isDone) {
+			yield return null;
+		}
+
+	}
+
+}
+
