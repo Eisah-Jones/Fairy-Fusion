47fe7fe13ba16349d08161c6c173d08342d3c344
Eisah Jones <jonesea@uci.edu>
2019-03-07 02:56:47 -0800

    Updated Options Menu

---
+++ 3 Barrel Shooter/Assets/Scripts/UI/UI_ButtonPress.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_ButtonPress : MonoBehaviour
+{
+    private Image image;
+    int i = 0;
+    int switchFrame = 60;
+
+    public Sprite imageIdle;
+    public Sprite imagePressed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        image = GetComponent<Image>();
+    }
+
+    void Update()
+    {
+        i++;
+        if (i < switchFrame)
+        {
+            image.sprite = imageIdle;
+        }
+        else
+        {
+            image.sprite = imagePressed;
+        }
+
+        if (i == switchFrame*2) i = 0;
+    }
+}
