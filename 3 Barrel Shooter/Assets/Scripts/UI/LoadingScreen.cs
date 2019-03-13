de5ae8de6353a9c499aa30eddef2a24cb053ed8e
Eisah Jones <jonesea@uci.edu>
2019-03-12 23:18:51 -0700

    Loading Scene Script

--- 3 Barrel Shooter/Assets/Scripts/UI/LoadingScreen.cs
+++ 3 Barrel Shooter/Assets/Scripts/UI/LoadingScreen.cs
@@ -4,29 +4,45 @@ using UnityEngine;
 
 public class LoadingScreen : MonoBehaviour
 {
-    public GameObject left;
-    public GameObject right;
+    private GameObject left;
+    private GameObject right;
+    private float speed = 1002f;
     public bool isOpen;
 
     private void Start()
     {
         left = transform.GetChild(0).gameObject;
         right = transform.GetChild(1).gameObject;
-        left.GetComponent<RectTransform>().localPosition = new Vector2(-800f, 0f);
-        right.GetComponent<RectTransform>().localPosition = new Vector2(800f, 0f);
+        left.GetComponent<RectTransform>().localPosition = new Vector2(-760f, 0f);
+        right.GetComponent<RectTransform>().localPosition = new Vector2(760f, 0f);
         isOpen = true;
+
+        //Close();
+        StartCoroutine("Close");
     }
 
 
-    public void Open()
+    public IEnumerator Open()
     {
-        if (isOpen) return;
+        while (!isOpen)
+        {
+            if (left.transform.localPosition.x >= -760) left.transform.localPosition += Vector3.left * speed * Time.deltaTime;
+            if (right.transform.localPosition.x <= 760) right.transform.localPosition += Vector3.right * speed * Time.deltaTime;
+            if ((int)left.transform.localPosition.x <= -760 && (int)right.transform.localPosition.x >= 760) isOpen = true;
+            yield return new WaitForEndOfFrame();
+        }
         isOpen = true;
     }
 
-    public void Close()
+    public IEnumerator Close()
     {
-        if (!isOpen) return;
+        while (isOpen)
+        {
+            if ((int) left.transform.localPosition.x <= -3) left.transform.localPosition += Vector3.right * speed * Time.deltaTime;
+            if ((int) right.transform.localPosition.x >= 3) right.transform.localPosition += Vector3.left * speed * Time.deltaTime;
+            if ((int) left.transform.localPosition.x >= 1 && (int)right.transform.localPosition.x <= -1) isOpen = false;
+            yield return new WaitForEndOfFrame();
+        }
         isOpen = false;
     }
 }
